07790c730dc65cb47c3d7a0dfb406e46d67ddb7b
Masita134 <maximoholstein@gmail.com>
2026-09-20 16:26:36 -0300

    feat: implementar actualizacion y eliminacion de perfil WAL-012

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,15 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
-
+use Illuminate\Database\Eloquent\SoftDeletes;   
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable , SoftDeletes;
 
     protected $fillable = [
         'name',
         'email',
         'password',
+        'age',
+        'image',
     ];
 
     protected $hidden = [
--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,10 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:api')->group(function () {
 
         Route::get('/profile', [ProfileController::class, 'show']);
+        
+        Route::put('/profile', [ProfileController::class, 'update']);
+        
+        Route::delete('/profile', [ProfileController::class, 'destroy']);
 
         Route::get('/account', [AccountController::class, 'show']);
         
